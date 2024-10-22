e6da0616652979b22f00d8a0bb499cab608e9192
ratent <ratent@taltech.ee>
2024-10-22 16:59:32 +0300

    Added yaml and swagger

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 require('express-async-errors');
 
+
 //extra security packages
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
+
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 const express = require('express');
 const app = express();
@@ -35,10 +41,11 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-
 app.get('/', (req, res) => {
-  res.send('Jobs api')
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
